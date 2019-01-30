791448200f70c8d678e4354b070c5c83bd7d4d63
l2l-k6 <1972054+l2l-k6@users.noreply.github.com>
2019-01-31 00:44:02 +0100

    Some more fiddling.

--- fb.py
+++ fb.py
@@ -2,33 +2,37 @@
 # -*- coding: utf-8 -*-
 
 import json
-import types
 
 seps = []
+uris = []
+guids = []
+
+def extract_guid(node, depth):
+	guids.append(node['guid'])
 
 def extract_sep(node, depth):
-	t = node['type']
-
-	if t == 'text/x-moz-place-separator':
+	if node['type'] == 'text/x-moz-place-separator':
 		seps.append(node)
+
+def extract_uri(node, depth):
+	if node['type'] == 'text/x-moz-place':
+		uris.append(node['uri'])
 
 def pprint(node, depth):
 	t = node['type']
 
 	if t == 'text/x-moz-place-separator':
-		print('\t' * depth + '---')
+		print('\t' * depth + str(node['index']) + '---')
 	elif t == 'text/x-moz-place':
-		print('\t' * depth + node['title'])
+		print('\t' * depth + str(node['index']) + ': ' + node['title'] + ': ' + node['uri'])
 	elif t == 'text/x-moz-place-container':
-		print('\t' * depth + '+' + node['title'])
+		print('\t' * depth + '+' + str(node['index']) + node['title'])
 	else:
 		print('Unkown type %s encountered' % t)
 
 def traverse(node, hook = lambda n, d,: None, depth = 0):
-
-	t = node['type']
 	hook(node, depth)
-	if t == 'text/x-moz-place-container' and 'children' in node:
+	if node['type'] == 'text/x-moz-place-container' and 'children' in node:
 		for c in node['children']:
 			traverse(c, hook, depth + 1)
 
@@ -39,5 +43,14 @@ if __name__ == '__main__':
 		data = json.load(f)
 
 	traverse(data, pprint)
-	traverse(data, extract_sep)
-	print(seps)
+	traverse(data, extract_guid)
+
+	seen = set()
+	uguids = []
+	for i in guids:
+		if i not in seen:
+			uguids.append(i)
+			seen.add(i)
+		else:
+			print('Duplicate guid: %s found!' % i)
+	print(uguids)
