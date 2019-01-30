4e0b73bf8af2ed9e4127b2d3bf295bd8fabe5e9a
l2l-k6 <1972054+l2l-k6@users.noreply.github.com>
2019-01-30 20:51:50 +0100

    Cleared up traverse function.

--- fb.py
+++ fb.py
@@ -6,31 +6,31 @@ import types
 
 seps = []
 
-def extract_sep(n, i):
-	seps.append(n)
-
-def pprint_title(n, i):
-	print('\t' * i + n['title'])
-
-def pprint_sep(n, i):
-	print('\t' * i + '---')
-
-def traverse(node, hook_place = lambda n, i: None,
-	hook_sep = lambda n, i: None, depth = 0):
-
+def extract_sep(node, depth):
 	t = node['type']
 
 	if t == 'text/x-moz-place-separator':
-		hook_sep(node, depth)
+		seps.append(node)
+
+def pprint(node, depth):
+	t = node['type']
+
+	if t == 'text/x-moz-place-separator':
+		print('\t' * depth + '---')
 	elif t == 'text/x-moz-place':
-		hook_place(node, depth)
+		print('\t' * depth + node['title'])
 	elif t == 'text/x-moz-place-container':
 		print('\t' * depth + '+' + node['title'])
-		if 'children' in node:
-			for c in node['children']:
-				traverse(c, hook_place, hook_sep, depth + 1)
 	else:
-		print('Unkown type %s encountered' % d['type'])
+		print('Unkown type %s encountered' % t)
+
+def traverse(node, hook = lambda n, d,: None, depth = 0):
+
+	t = node['type']
+	hook(node, depth)
+	if t == 'text/x-moz-place-container' and 'children' in node:
+		for c in node['children']:
+			traverse(c, hook, depth + 1)
 
 if __name__ == '__main__':
 
@@ -38,9 +38,6 @@ if __name__ == '__main__':
 	with open("bookmarks.json", "r") as f:
 		data = json.load(f)
 
-	traverse(data, pprint_title, pprint_sep)
-	        # if isinstance(data, types.DictType):
-		# 	print('True!')
-
-	traverse(data, hook_sep = extract_sep)
+	traverse(data, pprint)
+	traverse(data, extract_sep)
 	print(seps)
